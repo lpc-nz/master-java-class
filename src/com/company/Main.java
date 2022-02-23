eafe7c30b2dfc53e199e34206d08e79e095338c9
chung dev <chung@myovolt.com>
2022-02-23 22:02:22 +1300

    Variable and epxression

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    // variable with int data type
+        int myFirstNumber = 5;
+        int mySecondNumber = 10;
+        System.out.println(myFirstNumber);
+        System.out.println(mySecondNumber);
+
+        // Expression
+        int thirdNumber = 10;
+        int total = myFirstNumber + mySecondNumber + thirdNumber;
+        System.out.println(total);
     }
 }
