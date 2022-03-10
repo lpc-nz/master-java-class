d557709e163a82a3f82aa32a447eac404c4a77bc
chung dev <chung@myovolt.com>
2022-03-10 21:51:34 +1300

    Switch statement

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,28 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// Practice Switch statement
+        int myNumber = 1;
+
+        switch (myNumber){
+            case 1:
+                System.out.println("My number is 1 ");
+                break;
+            case 2:
+                System.out.println("My number is 2");
+                break;
+            case 3:
+                System.out.println("My number is 3");
+                break;
+            case 4:
+                System.out.println("My number is 4");
+                break;
+            case 5:
+                System.out.println("My number is 5");
+                break;
+            default:
+                System.out.println("Can not find number");
+
+        }
     }
 }
